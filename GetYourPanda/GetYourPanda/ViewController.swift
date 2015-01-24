612b95d9688251427cdc0f964452d33ab530d696
LittleCatBear <j.huguet9@gmail.com>
2015-01-24 18:47:58 +1300

    init project

---
+++ GetYourPanda/GetYourPanda/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GetYourPanda
+//
+//  Created by Julie Huguet on 24/01/2015.
+//  Copyright (c) 2015 Shokunin-Software. All rights reserved.
+//
+
+import UIKit
+
+class PictureViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
