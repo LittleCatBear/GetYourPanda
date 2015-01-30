79c5d83ad608b1701d51def1d0b5a6c6d9822e5a
LittleCatBear <j.huguet9@gmail.com>
2015-01-30 18:45:40 +1300

    finishing type part

--- GetYourPandaServer/app/controllers/types_controller.rb
+++ GetYourPandaServer/app/controllers/types_controller.rb
@@ -8,12 +8,30 @@ class TypesController < ApplicationController
 	end
 
 	def new
+		@type = Type.new
+	end
+
+	def edit
+		@type = Type.find(params[:id])
 	end
 
 	def create
 		@type = Type.new(type_params)
-		@type.save
-		redirect_to @type
+		if @type.save
+			redirect_to @type
+		else 
+			render "new"
+		end
+	end
+
+	def update
+		@type = Type.find(params[:id])
+
+		if @type.update(type_params)
+			redirect_to @type
+		else
+			render "edit"
+		end
 	end
 
 	def destroy
