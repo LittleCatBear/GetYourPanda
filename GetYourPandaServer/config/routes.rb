55c8834264a2e9da216a3ef86b6a442d307db46b
LittleCatBear <j.huguet9@gmail.com>
2015-01-30 17:29:03 +1300

    new controller types

--- GetYourPandaServer/config/routes.rb
+++ GetYourPandaServer/config/routes.rb
@@ -5,7 +5,13 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+
+  # resources declaration
+  resources :types do
+    resources :images
+  end
+  
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
