fedfa622e5a5a32cedd31584cba53a7947cee1e8
LittleCatBear <j.huguet9@gmail.com>
2015-01-30 21:05:34 +1300

    image management

--- GetYourPandaServer/app/controllers/images_controller.rb
+++ GetYourPandaServer/app/controllers/images_controller.rb
@@ -1,2 +1,20 @@
 class ImagesController < ApplicationController
+
+	def create
+		@type = Type.find(params[:type_id])
+		@image = @type.images.create(image_params)
+		redirect_to type_path(@type)
+	end
+
+	def destroy
+		@type = Type.find(params[:type_id])
+		@image = @type.images.find(params[:id])
+		@image.destroy
+		redirect_to type_path(@type)
+	end
+
+	private
+		def image_params
+			params.require(:image).permit(:url)
+		end
 end
--- GetYourPandaServer/app/models/type.rb
+++ GetYourPandaServer/app/models/type.rb
@@ -1,4 +1,5 @@
 class Type < ActiveRecord::Base
+	has_many :images, dependent: :destroy
 	validates :name, presence: true,
                     length: { minimum: 3 }
 end
