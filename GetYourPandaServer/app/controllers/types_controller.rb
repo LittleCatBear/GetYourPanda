6bd59fd1863dbfb46a0a50bb79d6ab70fb427588
LittleCatBear <j.huguet9@gmail.com>
2015-01-30 18:25:35 +1300

    adding links

--- GetYourPandaServer/app/controllers/types_controller.rb
+++ GetYourPandaServer/app/controllers/types_controller.rb
@@ -1,16 +1,30 @@
 class TypesController < ApplicationController
 
-	#new type
-	def show
+	def index
+		@types = Type.all
 	end
-	
+	def show
+		@type = Type.find(params[:id])
+	end
+
 	def new
 	end
 
 	def create
+		@type = Type.new(type_params)
+		@type.save
+		redirect_to @type
 	end
 
 	def destroy
+		@type = Type.find(params[:id])
+		@type.destroy
+		redirect_to 
 	end
+
+	private
+		def type_params
+			params.require(:type).permit(:name)
+		end
 
 end
--- GetYourPandaServer/app/models/type.rb
+++ GetYourPandaServer/app/models/type.rb
@@ -1,2 +1,4 @@
 class Type < ActiveRecord::Base
+	validates :name, presence: true,
+                    length: { minimum: 3 }
 end
