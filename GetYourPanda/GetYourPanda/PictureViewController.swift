6c81c7cd2c95ae90d0ced96a95c1c16e99750eaa
LittleCatBear <j.huguet9@gmail.com>
2015-02-02 15:18:24 +1300

    add saving photo feature

--- GetYourPanda/GetYourPanda/PictureViewController.swift
+++ GetYourPanda/GetYourPanda/PictureViewController.swift
@@ -7,21 +7,35 @@
 //
 
 import UIKit
+import AssetsLibrary
 
 class PictureViewController: UIViewController, UIApplicationDelegate {
     
     @IBOutlet weak var customImageView: UIImageView!
     let pictureServices : PictureServices = PictureServices()
     
+    var assetsLibrary: ALAssetsLibrary?
+    typealias CompletionHandler = (success:Bool!) -> Void
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        assetsLibrary = ALAssetsLibrary()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //save to assets library in custom folder named "GYP_Album"
+    @IBAction func saveClickButton(sender: UIButton) {
+        var orientation:ALAssetOrientation = ALAssetOrientation.Up
+        var image = customImageView.image
+        self.addImage(image!, toAlbum: "GYP_Album", orientation:orientation) { (success) -> Void in
+            print("Image Added : \(success)");
+        }
+
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -34,8 +48,54 @@ class PictureViewController: UIViewController, UIApplicationDelegate {
         println("test")
     }
     
-    
-
+    // Create or check existing group for picture & save picture in the group
+    func addImage(image:UIImage, toAlbum albumName:String, orientation : ALAssetOrientation, handler:CompletionHandler){
+        
+        assetsLibrary?.addAssetsGroupAlbumWithName(albumName, resultBlock: {(group:ALAssetsGroup!) -> Void in
+            print("\nAlbum Created:=  \(albumName)");
+            /*-- Find Group --*/
+            
+            var groupToAddTo:ALAssetsGroup?;
+            
+            self.assetsLibrary?.enumerateGroupsWithTypes(ALAssetsGroupType(ALAssetsGroupAlbum),
+                usingBlock: { (group:ALAssetsGroup?, stop:UnsafeMutablePointer<ObjCBool>) -> Void in
+                    
+                    if(group != nil){
+                        
+                        if group!.valueForProperty(ALAssetsGroupPropertyName) as String == albumName{
+                            groupToAddTo = group;
+                            
+                            print("\nGroup Found \(group!.valueForProperty(ALAssetsGroupPropertyName))\n");
+                            
+                            self.assetsLibrary?.writeImageToSavedPhotosAlbum(image.CGImage, orientation: orientation, completionBlock: {(assetURL:NSURL!,error:NSError!) -> Void in
+                                
+                                if(error == nil){
+                                    self.assetsLibrary?.assetForURL(assetURL,
+                                        resultBlock: { (asset:ALAsset!) -> Void in
+                                            var yes:Bool? = groupToAddTo?.addAsset(asset);
+                                            if (yes == true){
+                                                handler(success: true);
+                                            }
+                                        },
+                                        failureBlock: { (error2:NSError!) -> Void in
+                                            print("Failed to add asset");
+                                            handler(success: false);
+                                    });
+                                }
+                            });
+                        }
+                    } /*Group Is Not nil*/
+                },
+                failureBlock: { (error:NSError!) -> Void in
+                    print("Failed to find group");
+                    handler(success: false);
+            });
+            
+            }, failureBlock: { (error:NSError!) -> Void in
+                print("Failed to create \(error)");
+                handler(success: false);
+        });
+    }
 
 }
 
